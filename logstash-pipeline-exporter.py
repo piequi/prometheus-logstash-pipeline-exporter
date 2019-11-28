6074b8014ac4a62e899ebf667cc6515ee5235bd8
Marc-Antoine BOURGEOT <mbourgeot.ext@orange.com>
2019-11-28 20:20:51 +0100

    Add default values to usage output

--- logstash-pipeline-exporter.py
+++ logstash-pipeline-exporter.py
@@ -175,7 +175,7 @@ class LogstashPipelineCollector(object):
 if __name__ == "__main__":
 
     # parse command line arguments
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-d", "--debug", action="store_true", help="set debug mode")
     parser.add_argument("logstash_endpoint", help="logstash's endpoint", nargs='?', default="http://localhost:9600")
     parser.add_argument("web_listen_address", help="exporter binding address and port", nargs='?',
